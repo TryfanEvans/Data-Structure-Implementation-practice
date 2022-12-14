1238cc8e7b37e6d051a9099ba1f22a6f13bd3d53
TryfanEvans <TryfanLE@gmail.com>
2022-12-14 13:50:27 +1030

    init

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "binary_tree.h"
+#include "breadth_first_search.h"
+#include "depth_first_search.h"
+#include "graph_using_an_adjacency_list.h"
+#include "linked_list.h"
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
